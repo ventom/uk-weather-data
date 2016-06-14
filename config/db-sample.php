ee736485ff96c9fcc00bc6fb2963f883635c8656
Ventsislav Tomev <ventsislav.tomev@ventom-solutions.com>
2016-06-14 14:40:30 +0300

    DB config sample

---
+++ config/db-sample.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=uk_weather',
+    'username' => 'root',
+    'password' => '',
+    'charset' => 'utf8',
+];
